3d7cefdbee0f275c72b76475985aa2a0eda000b9
root <davi2812@hotmail.com>
2025-03-04 21:01:09 +0000

    improved devcontainer setting

--- cmake/sycl_toolchain.cmake
+++ cmake/sycl_toolchain.cmake
@@ -1,18 +1,10 @@
 # Set the target architecture
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
+# set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # Specify the cross-compilation tools (clang as the compiler)
-# set(CMAKE_C_COMPILER /opt/bin/acpp)
+set(CMAKE_C_COMPILER /usr/bin/clang)
 set(CMAKE_CXX_COMPILER /opt/sycl/bin/acpp)
-
-# set(CMAKE_C_COMPILER clang)
-# set(CMAKE_CXX_COMPILER clang++)
-# set(CMAKE_C_FLAGS "--target=aarch64-linux-gnu -march=armv8-a")
-# set(CMAKE_CXX_FLAGS "--target=aarch64-linux-gnu -march=armv8-a")
-
-# Specify the path to the cross-compilation libraries and headers
-# set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
 
 # Ensure that CMake will search in the cross-compilation environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
