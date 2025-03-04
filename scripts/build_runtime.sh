81574bbe643738c3d25b91b56f41f30aadd94846
root <davi2812@hotmail.com>
2025-03-04 22:48:19 +0000

    improved information on building with AoT cuda

--- scripts/build_runtime.sh
+++ scripts/build_runtime.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Build a runtime docker to be pulled by the target.
 
-
 RUNTIME_IMAGE_TAG="mdnf1992/sycl-runtime"
 
 docker buildx build  -f .devcontainer/Dockerfile --target runtime -t $RUNTIME_IMAGE_TAG .
