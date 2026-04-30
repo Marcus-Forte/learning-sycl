131bd25171ebcd1de5c161502ce03dd42c73a8aa
Marcus Forte <davi2812@hotmail.com>
2026-04-30 18:21:07 +0200

    new base

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,10 @@
 # Build SYCL Docker images
 
 # Default (Lapack)
-docker build --push -f .devcontainer/Dockerfile --target onemath_lapack -t mdnf1992/sycl-dev .
+docker build --push -f .devcontainer/Dockerfile --build-arg BASE_IMG=mdnf1992/cpp-dev --target onemath_lapack -t mdnf1992/sycl-dev .
 
 # Default (Generic)
-docker build --push -f .devcontainer/Dockerfile --target onemath_generic -t mdnf1992/sycl-dev:generic .
+docker build --push -f .devcontainer/Dockerfile --build-arg BASE_IMG=mdnf1992/cpp-dev --target onemath_generic -t mdnf1992/sycl-dev:generic .
 
 # Tegra (Cuda)
 docker build --push -f .devcontainer/Dockerfile --build-arg BASE_IMG=nvcr.io/nvidia/l4t-cuda:12.2.12-devel --target onemath_cublas -t mdnf1992/sycl-dev:tegra .
