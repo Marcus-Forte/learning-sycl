5609d5a708e49c1fda8677145c27f1eef02ea05c
Marcus Forte <davi2812@hotmail.com>
2026-05-03 19:47:15 +0200

    updated dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -5,8 +5,11 @@
 # Default (Lapack)
 docker build --push -f .devcontainer/Dockerfile --build-arg BASE_IMG=mdnf1992/cpp-dev --target onemath_lapack -t mdnf1992/sycl-dev .
 
+# Default (Lapack) + Cuda
+docker build --push -f .devcontainer/Dockerfile --build-arg SYCL_CUDA_FLAG="--cuda" --build-arg BASE_IMG=mdnf1992/cpp-dev --target onemath_lapack -t mdnf1992/sycl-dev .
+
 # Default (Generic)
 docker build --push -f .devcontainer/Dockerfile --build-arg BASE_IMG=mdnf1992/cpp-dev --target onemath_generic -t mdnf1992/sycl-dev:generic .
 
 # Tegra (Cuda)
-docker build --push -f .devcontainer/Dockerfile --build-arg BASE_IMG=nvcr.io/nvidia/l4t-cuda:12.2.12-devel --target onemath_cublas -t mdnf1992/sycl-dev:tegra .
+docker build --push -f .devcontainer/Dockerfile --build-arg BASE_IMG=nvcr.io/nvidia/l4t-cuda:12.2.12-devel --build-arg SYCL_CUDA_FLAG="--cuda" --target onemath_cublas -t mdnf1992/sycl-dev:tegra .
